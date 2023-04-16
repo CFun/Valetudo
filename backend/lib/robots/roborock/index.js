165a2f34ea5a9f986546d17f6c509b39089902a1
Stefan Damm <stefan@autama.de>
2023-04-16 23:46:38 +0200

    Add S7ProUltra Model

--- backend/lib/robots/roborock/index.js
+++ backend/lib/robots/roborock/index.js
@@ -9,5 +9,6 @@ module.exports = {
     "RoborockS6PureValetudoRobot": require("./RoborockS6PureValetudoRobot"),
     "RoborockS6ValetudoRobot": require("./RoborockS6ValetudoRobot"),
     "RoborockS7ValetudoRobot": require("./RoborockS7ValetudoRobot"),
+    "RoborockS7ProUltraValetudoRobot": require("./RoborockS7ProUltraValetudoRobot"),
     "RoborockV1ValetudoRobot": require("./RoborockV1ValetudoRobot")
 };
